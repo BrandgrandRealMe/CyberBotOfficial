11e79f56f962c2a17f62bad0849fd7af9174a184
BrandgrandReal <47084010+BrandgrandRealMe@users.noreply.github.com>
2024-05-13 15:08:43 +0000

    Fixes?

--- slashCommands/utility/announce.js
+++ slashCommands/utility/announce.js
@@ -62,8 +62,8 @@ module.exports = {
     const RAWimg = interaction.options.getAttachment("image");
 
     if (!announcementChannel.permissionsFor(muser).has(PermissionsBitField.Flags.SendMessages)) return interaction.editReply({ content: "You don't have permission to send messages in that channel." });
-    console.log(interaction.guild.me)
-    if (!announcementChannel.permissionsFor(interaction.guild.me).has(PermissionsBitField.Flags.SendMessages)) return interaction.editReply({ content: "I don't have permission to send messages in that channel." });
+    console.log(interaction.guild.members.me)
+    if (!announcementChannel.permissionsFor(interaction.guild.members.me).has(PermissionsBitField.Flags.SendMessages)) return interaction.editReply({ content: "I don't have permission to send messages in that channel." });
     console.log("Sending announcement to " + announcementChannel.name);
     try {
       var title = RAWtitle.value;
