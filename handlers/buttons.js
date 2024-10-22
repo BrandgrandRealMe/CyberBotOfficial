12a7401092e2769a483f609b59101e557d191295
BrandgrandRealMe <brandon@brandgrand.tech>
2024-10-22 15:31:27 -0500

    test

--- handlers/buttons.js
+++ handlers/buttons.js
@@ -6,7 +6,7 @@ var table = new AsciiTable()
 table.setHeading('Buttons', 'Stats').setBorder('|', '=', "0", "0")
 
 module.exports = (client) => {
-  const buttonsPath = path.join(__dirname, '..', 'buttons');
+  const buttonsPath = path.join(__dirname, '...', 'buttons');
     fs.readdirSync(buttonsPath).filter((file) => file.endsWith('.js')).forEach((file) => {
         const button = require(`${buttonsPath}/${file}`)
         client.buttons.set(button.id, button)
