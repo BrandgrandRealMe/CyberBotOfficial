ac53104ef0836d225598e392c33a752897607893
BrandgrandRealMe <brandon@brandgrand.tech>
2024-10-22 15:30:01 -0500

    fix hopefully

--- handlers/buttons.js
+++ handlers/buttons.js
@@ -6,8 +6,9 @@ var table = new AsciiTable()
 table.setHeading('Buttons', 'Stats').setBorder('|', '=', "0", "0")
 
 module.exports = (client) => {
-    fs.readdirSync("../buttons").filter((file) => file.endsWith('.js')).forEach((file) => {
-        const button = require(`../buttons/${file}`)
+  const buttonsPath = path.join(__dirname, '..', 'buttons');
+    fs.readdirSync(buttonsPath).filter((file) => file.endsWith('.js')).forEach((file) => {
+        const button = require(`${buttonsPath}/${file}`)
         client.buttons.set(button.id, button)
 		table.addRow(button.id, '✅')
     })
